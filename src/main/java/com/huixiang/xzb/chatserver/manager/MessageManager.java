ac6d1261c3391ee0fb604ccf015d5bc5f99e5070
Dalio <2267264176@qq.com>
2019-05-25 15:34:22 +0800

    Update Architecture - use redis to save offline message

--- src/main/java/com/huixiang/xzb/chatserver/manager/MessageManager.java
+++ src/main/java/com/huixiang/xzb/chatserver/manager/MessageManager.java
@@ -1,10 +1,11 @@
 package com.huixiang.xzb.chatserver.manager;
 
 /**
- * When send message to a offline user, I call the offline user first,
- * then save the messages into mysql.
+ * When send messages to a offline user, I call the offline user first,
+ * then save the messages in redis set.
  *
- * When the user go online, send these messages to him first
+ * When the user go online, send these messages saved previous to him first
  */
 public class MessageManager {
+
 }
